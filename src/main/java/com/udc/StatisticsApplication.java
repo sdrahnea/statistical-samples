99bc8e0a5ff1b2599034456366e9b3ca73074599
Sergiu Drahnea <s.drahnea@gmail.com>
2020-01-29 11:58:54 +0200

    add .gitignore file some changes

--- src/main/java/com/udc/StatisticsApplication.java
+++ src/main/java/com/udc/StatisticsApplication.java
@@ -9,9 +9,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
-public class SpringBootJpaMySqlPrimefacesTemplateApplication {
+public class StatisticsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootJpaMySqlPrimefacesTemplateApplication.class, args);
+        SpringApplication.run(StatisticsApplication.class, args);
     }
 }
